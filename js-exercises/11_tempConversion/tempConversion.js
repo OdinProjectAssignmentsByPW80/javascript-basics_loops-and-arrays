34a852311e7b3fe1ed85dfafbd4076c9338ba79a
Paul Williams <advanced_solution_delivery@outlook.com>
2025-06-06 12:07:28 +0100

    feat: tempConversion
    Changes to be committed:
    	modified:   js-exercises/11_tempConversion/tempConversion.js
    	modified:   js-exercises/11_tempConversion/tempConversion.spec.js

--- js-exercises/11_tempConversion/tempConversion.js
+++ js-exercises/11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  return roundToOneDP(((fahrenheit - 32) * 5) / 9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  return roundToOneDP((celsius * 9) / 5 + 32);
+};
+
+const roundToOneDP = function (num) {
+  return Math.round(num * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
